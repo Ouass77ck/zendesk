f5f1dfa03d57df5d3b824e8f22cb3b6d11184f69
Lauranne Fossat <laurannefossat10@gmail.com>
2025-03-31 11:50:51 +0300

    Suppression des fichiers de build suivis par erreur

--- .pio/build/esp32/FrameworkArduino/firmware_msc_fat.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32/FrameworkArduino/firmware_msc_fat.c.o: \
- /home/wassim/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
- /home/wassim/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
--- .pio/build/esp32/FrameworkArduino/libb64/cencode.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32/FrameworkArduino/libb64/cencode.c.o: \
- /home/wassim/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.c \
- /home/wassim/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.h
